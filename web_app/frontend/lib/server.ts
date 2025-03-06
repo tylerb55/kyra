4d679603ca7fde3101039add71fc3d0cf690d30a
tylerb55 <hmtylerb@hotmail.co.uk>
2025-03-06 20:35:19 +0000

    quick fix

--- web_app/frontend/lib/server.ts
+++ web_app/frontend/lib/server.ts
@@ -1,5 +1,5 @@
-export const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL;
-export const SUPABASE_ANON_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+export const SUPABASE_URL: string = process.env.NEXT_PUBLIC_SUPABASE_URL ?? "";
+export const SUPABASE_ANON_KEY: string = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ?? "";
 
 if (!SUPABASE_URL || !SUPABASE_ANON_KEY) {
     throw new Error("Missing Supabase environment variables!");
